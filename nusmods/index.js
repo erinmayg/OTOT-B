54ee3f6c59154628593ea9525208f699d4760f1e
erinmayg <erinmaygunawan@gmail.com>
2022-10-17 00:09:05 +0800

    Add nusmods azure function

---
+++ nusmods/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  let { AY, faculty, department } = req.query;
+  AY = req.query.ay || '2022-2023';
+
+  let modules = await axios
+    .get(`https://api.nusmods.com/v2/${AY}/moduleInfo.json`)
+    .then((res) => res.data);
+
+  if (faculty) {
+    modules = modules.filter((module) => module.faculty == faculty);
+  }
+
+  if (department) {
+    modules = modules.filter((module) => module.department == department);
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: modules,
+  };
+};
