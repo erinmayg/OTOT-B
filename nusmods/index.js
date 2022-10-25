60ff324e04645eb57c49a348e8839e962ea249f6
Erin May Gunawan <60438326+erinmayg@users.noreply.github.com>
2022-10-25 23:02:14 +0800

    Add serverless function (#2)
    * Add nusmods azure function
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Add cors header
    
    * Add faculties and departments

---
+++ nusmods/index.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  let { AY, faculty, department } = req.query;
+  AY = req.query.ay || '2022-2023';
+
+  let modules = await axios
+    .get(`https://api.nusmods.com/v2/${AY}/moduleInfo.json`)
+    .then((res) => res.data);
+
+  if (faculty) {
+    modules = modules.filter((module) => module.faculty == faculty);
+  }
+
+  if (department) {
+    modules = modules.filter((module) => module.department == department);
+  }
+
+  let faculties = [...new Set(modules.map((module) => module.faculty))];
+  let departments = [...new Set(modules.map((module) => module.department))];
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: { faculties, departments, modules },
+  };
+};
