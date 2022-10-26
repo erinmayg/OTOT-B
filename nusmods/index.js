861ae66123254f1611a4d02e76302bb651f33391
erinmayg <erinmaygunawan@gmail.com>
2022-10-26 15:58:10 +0800

    Implement pagination

--- nusmods/index.js
+++ nusmods/index.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
-  let { AY, faculty, department } = req.query;
+  let { AY, faculty, department, page } = req.query;
+  page = req.query.page || 1;
   AY = req.query.ay || '2022-2023';
 
   let modules = await axios
@@ -21,12 +22,20 @@ module.exports = async function (context, req) {
   let faculties = [...new Set(modules.map((module) => module.faculty))];
   let departments = [...new Set(modules.map((module) => module.department))];
 
+  pages = Math.ceil(modules.length / 8);
+  modules = modules.splice(8 * (page - 1), 8 * page);
+
   context.res = {
     // status: 200, /* Defaults to 200 */
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     },
-    body: { faculties, departments, modules },
+    body: {
+      faculties,
+      departments,
+      pages,
+      modules,
+    },
   };
 };
